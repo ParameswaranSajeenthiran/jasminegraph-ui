9e5e38a0ee7236df436ff1aa10fd9d504a6e51c1
ParameswaranSajeenthiran <psajeendran@gmail.com>
2026-02-14 17:09:00 +0530

    Add rag/page.tsx

---
+++ Frontend/src/app/rag/page.tsx
@@ -0,0 +1,12 @@
+"use client";
+
+import PageWrapper from "@/layouts/page-wrapper";
+import GraphRagLayout from "@/components/rag/GraphRagLayout";
+
+export default function GraphRagPage() {
+  return (
+      <PageWrapper>
+        <GraphRagLayout />
+      </PageWrapper>
+  );
+}
